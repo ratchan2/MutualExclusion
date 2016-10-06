517742affc86befcb89898e99958912efe62e353
Sarat Chandra <sarat.vrns@gmail.com>
2016-10-06 16:49:11 -0500

    Tournament updated
    Change-Id: If1a44dc6919e1a38a4a4d6b71bc892803c4f7be8

--- Tournament.java
+++ Tournament.java
@@ -31,33 +31,15 @@ class Peterson{
 public class Tournament implements Callable<Integer>{
 
 	public static int n;
-	public static int height;
-	public volatile static boolean[][] flags;
-	public volatile static int victims[];
-	public volatile static int access[][];
-	public volatile static boolean inCS = false;
-	public volatile static int idCS = -1;
-	public volatile static int violation = 0;
 	public volatile static ArrayList<Peterson> petersonLocks = new ArrayList<Peterson>();
-	public static ArrayList<ArrayList<Tuple>> tupleList = new ArrayList<ArrayList<Tuple>>(16);
 	public static void init(int size){
 		n = size;
-		int levels = (int)(Math.ceil(Math.log(size)/ Math.log(2)));
-		Double h = Math.ceil(Math.log(size) / Math.log(2) );
-		height =  h.intValue();
-	    flags = new boolean[(int)(Math.pow(2, levels)) - 1][2];
-	    access = new int[(int)(Math.pow(2, levels)) - 1][2];
-	    victims = new int[(int)(Math.pow(2, levels)) - 1];
-	    
-	    for(int i = 0; i < n; i++){
-	    	ArrayList<Tuple> list = new ArrayList<Tuple>();
-	    	tupleList.add(list);
-	    }
+		levels = (int)(Math.ceil(Math.log(size)/Math.log(2)));
+    	if(levels == 0){
+    		levels = 1;
+    	}
+		
 	    for(int i = 0; i < (int)(Math.pow(2, levels)) - 1 ; i++){
-	    	flags[i][0] = false;
-	    	flags[i][1] = false;
-	    	access[i][0] = -1;
-	    	access[i][1] = -1;
 	    	petersonLocks.add(new Peterson());
 	    }	
 	    
@@ -77,27 +59,17 @@ public class Tournament implements Callable<Integer>{
 	     this.addr = id;
 	     list.add(this);
 	}
-	public static  void setFlagAndVictim(int currentLock, int id){
-		 flags[currentLock][id] = true;
-		 victims[currentLock] = id;
-	}
+	
 	
     public void lock(){
-         int locksAcquired = 0;
+       
+    	int locksAcquired = 0;
     	 while(locksAcquired < levels){
             int currentLock = traceLock[locksAcquired];
             int id = traceId[locksAcquired];
-            
-//    		while(access[currentLock][id] != addr || flags[currentLock][id]){
-//            	   while(flags[currentLock][id]);      
-//            	    access[currentLock][id] = addr;  
-//            }
-//             
-//       		flags[currentLock][id] = true;
-//       		victims[currentLock] = addr;
-//    		 while( flags[currentLock][1 - id] &&  victims[currentLock] == addr);
-  
-               petersonLocks.get(currentLock).lock(addr, id);
+              
+               //an instance of peterson's lock
+               petersonLocks.get(currentLock).lock(addr,id);
     		   locksAcquired++;
     		   
     		   	
@@ -106,10 +78,9 @@ public class Tournament implements Callable<Integer>{
     }
     
     public static void assignLeafLocks(int size){
-    	levels = (int)(Math.ceil(Math.log(size)/Math.log(2)));
-    	System.out.println("Levels: " + levels);
-    	int leaf = (int)(Math.pow(2, levels -1)) - 1;
     	
+    	int leaf = (int)(Math.pow(2, levels -1)) - 1;
+   
     	for(int i = 0; i < list.size(); i++){
     		list.get(i).traceLock = new int[levels];
     		list.get(i).traceId = new int[levels];
@@ -129,6 +100,7 @@ public class Tournament implements Callable<Integer>{
     	 
     }
     public void buildTraceLocks(){
+    	//every thread knows all the locks it needs to acquire from leaflock all the way upto the parent
     	  int currentLock = leafLock;
           int id = (leafLeft) ? 0 : 1;
           traceLock[0] = currentLock;
@@ -145,15 +117,7 @@ public class Tournament implements Callable<Integer>{
            traceLock[i] = currentLock;
            traceId[i] = id;
     	  }
-    	  String line = addr + ">";
-    	  for(int i = 0; i < levels; i++){
-    		  line+= " (" + traceLock[i] + "," + traceId[i] + ")";
-    		  if(i < levels - 1){
-    			  line += ",";
-    		  }
-    	  }
-    	  System.out.println(line);
-    }
+  }
     public void begin(){
     	   int count = 0;
     	   while(count < csCount){
@@ -164,60 +128,45 @@ public class Tournament implements Callable<Integer>{
     	   }
     }
     public void cs(){
-       int id;
-      if( (id = idCS) != -1){
-    		//log!
-    		System.out.println("GOTCHA!!" + "addr: " + addr + ", idCS: " + id + ",counter: "+ counter);
-    		violation++;
-    	}
-    	inCS = true;
-    	idCS = addr;
     	counter++;   
-    	idCS = -1;
-    	tupleList.get(addr).add(new Tuple(traceLock,traceId));
     		
     }
    
     public static  void  unlock(int traceLock[],int[] traceId){
     
     	   for(int i = levels - 1 ; i >= 0; i-- ){
-    		   //  flags[traceLock[i]][traceId[i]] = false;
     		   petersonLocks.get(traceLock[i]).unlock(traceId[i]);
     	   }
-    	   
-    	   
-    //	TreeLock.flags[traceLock[levels -1 ]][traceId[levels -1]] = false;
     }
 	
 	public Integer call(){
 		begin();
-	//	System.out.println(addr + ", counter: " + counter);
 		return new Integer(0);
 	}
 	
 	public static void main(String args[]){
+	
 		Runtime runtime = Runtime.getRuntime();
-		System.out.println(runtime.availableProcessors());
 		int size = Integer.parseInt(args[0]);
 		Tournament.csCount = Integer.parseInt(args[1]);
+		
+		//All intializations here
 		for(int i = 0; i < size; i++){
 			new  Tournament(i);
 		}
-		 init(size);
+		init(size);
 		Tournament.assignLeafLocks(size);
 		for(int i = 0; i < size; i++){
 			Tournament.list.get(i).buildTraceLocks();
 		}
-		for(int i = 0; i < Tournament.list.size();i++){
-		//	 System.out.println(i +" , leaf: " + list.get(i).leafLock);
-		}
-		
+		//Initializations done!
 		
 		ForkJoinPool pool = new ForkJoinPool();
 	    List<Future<Integer>> results  = new ArrayList<Future<Integer>>(); 
 	    
 	    
 	    long startTime = System.nanoTime();
+	   
 	    results = pool.invokeAll(Tournament.list);
 	    
 	    boolean bResult = false;
@@ -230,7 +179,7 @@ public class Tournament implements Callable<Integer>{
 	    }
 	    long endTime = System.nanoTime();
 	    
-	    System.out.println("Count: " + counter + ",Violation: " + violation + ", Time: " + (endTime - startTime));
+	    System.out.println("Count: " + counter + ", Time: " + (endTime - startTime));
 	 	
 	}
 }
